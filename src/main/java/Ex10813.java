de85e54e77d18ea71d61438900eedd039b391941
JeongukChoi <onsucjuk@gmail.com>
2023-03-24 08:59:13 +0900

    Bj Ex10813

--- src/main/java/Ex10813.java
+++ src/main/java/Ex10813.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Ex10813 {
-}
+import java.util.Scanner;
+public class Ex10813 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int[] arr = new int[a];
+
+        for (int i = 0; i < a; i++) {
+
+            arr[i] = i + 1;
+
+        }
+
+        for (int j = 0; j < b; j++) {
+
+            int c = sc.nextInt();
+            int d = sc.nextInt();
+
+            int e = arr[c - 1];
+            int f = arr[d - 1];
+
+            arr[c - 1] = f;
+            arr[d - 1] = e;
+
+        }
+
+        for (int k = 0; k < a; k++) {
+
+            System.out.print(arr[k] + " ");
+        }
+    }
+
