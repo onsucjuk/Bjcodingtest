55d1de9a189a47d7f44a4726ad14ae73633f70aa
JeongukChoi <onsucjuk@gmail.com>
2023-03-29 08:57:04 +0900

    Bj Ex27866ver2

---
+++ src/main/java/Ex27866ver2.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex27866ver2 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String st = br.readLine().trim();
+        int num = Integer.parseInt(br.readLine());
+
+        System.out.println(st.substring(num-1, num));
+        // st.chatAt(num-1); 대체 사용 가능
+
+
+
+    }
+}
