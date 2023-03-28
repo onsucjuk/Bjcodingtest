ba8bf983bcfec3e1e5441cb7b4fcedbf9b25e58a
JeongukChoi <onsucjuk@gmail.com>
2023-03-29 08:46:13 +0900

    Bj Ex27866

---
+++ src/main/java/Ex27866.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ex27866 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String[] st = sc.next().split("");
+
+        System.out.println(st[sc.nextInt()-1]);
+
+    }
+}
