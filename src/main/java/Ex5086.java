2ff670418e34f9b30374477902e85c621a1b7610
JeongukChoi <onsucjuk@gmail.com>
2023-04-06 12:53:39 +0900

    2023.04.06 약수(factor) 배수(multiple) 약수배수 아닌 것(neither) 구분 백준 5086번 문제

---
+++ src/main/java/Ex5086.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Ex5086 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+
+            float a = (float) sc.nextInt();
+            float b = (float) sc.nextInt();
+            int c = (int) a;
+            int d = (int) b;
+            float f = c / (float)d;
+            float e = c / d;
+
+            if (a == 0)
+                break;
+
+            if (a/b > 1 && a/b == e) {
+                System.out.printf("multiple\n");
+            }  else if (a/b < 1 && a/b == f) {
+                System.out.printf("factor\n");
+            } else {
+                System.out.printf("neither\n");
+            }
+        }
+        sc.close();
+    }
+}
