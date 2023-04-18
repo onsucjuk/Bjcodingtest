d87fd85e633f2ea164e9fa90bb51008b503e800d
JeongukChoi <onsucjuk@gmail.com>
2023-04-19 08:29:42 +0900

    2023.04.19 백준 3003번 체스말 갯수 찾기(6종류)

--- src/main/java/Bj_intensification/Ex_3003.java
+++ src/main/java/Bj_intensification/Ex_3003.java
@@ -14,14 +14,14 @@ public class Ex_3003 {
 
         for (int i = 0; i < arr.length; i++) {
                 if(i<2) { // (킹,퀸), (나이트 비숍, 룩), (폰)으로 갯수 별로 나눠서 배열 저장하면서 인수 받아서 갯수 갱신
-                        arr[i] = 1 - Integer.parseInt(st.nextToken());
-                        System.out.print(arr[i]+" "); // 구분 " "포함 출력
+                    arr[i] = 1 - Integer.parseInt(st.nextToken());
+                    System.out.print(arr[i]+" "); // 구분 " "포함 출력
                     } else if(i<5) {
-                            arr[i] = 2 - Integer.parseInt(st.nextToken());
-                            System.out.print(arr[i]+" ");
+                        arr[i] = 2 - Integer.parseInt(st.nextToken());
+                        System.out.print(arr[i]+" ");
                         } else {
-                                arr[i] = 8 - Integer.parseInt(st.nextToken());
-                                System.out.print(arr[i]); // 마지막 출력(폰 갯수)시 갯수만 출력
+                            arr[i] = 8 - Integer.parseInt(st.nextToken());
+                            System.out.print(arr[i]); // 마지막 출력(폰 갯수)시 갯수만 출력
                             }
         }
     }
